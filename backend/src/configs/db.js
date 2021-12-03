0ff4afcceb15f9a2f92b6c7f91fef9dc4b6b9dd0
Enzo FATEC <enzo.gerola@fatec.sp.gov.br>
2021-12-03 08:10:12 -0300

    # Backend
    - Alterando host (mongoDB)

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/qualicorp_eg_db')
+mongoose.connect('mongodb://localhost:27017/qualicorp_eg_db')
 var Schema = mongoose.Schema
 
 var clientDataSchema = new Schema({
